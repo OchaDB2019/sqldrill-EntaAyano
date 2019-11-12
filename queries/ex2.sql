fd3f64dad62a027ebf43d7cf217693992e69b533
EntaAyano <57655215+EntaAyano@users.noreply.github.com>
2019-11-13 00:24:32 +0900

    Update ex2.sql

--- queries/ex2.sql
+++ queries/ex2.sql
@@ -2,6 +2,6 @@
 # 'SHARP シャープ株式会社' さんをフォローする人のアカウント名を求めよ。
 # この下の行にSQL文を書きましょう。
 
-SELECT follower account
-FROM follow.f
-WHERE followee account = 'SHARP_JP'
+SELECT f.follower_account
+FROM follow f
+WHERE f.followee_account = 'SHARP_JP'
